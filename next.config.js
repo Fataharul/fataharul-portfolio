8d93db8dd53d03880965892817f92273bfa7e03c
fataharul <fataharul@gmail.com>
2026-02-08 00:21:17 +0600

    Modified nextConfig

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: isGithubPages ? "/portfolio-fataharul" : "",
-  assetPrefix: isGithubPages ? "/portfolio-fataharul/" : "",
 };
 
 module.exports = nextConfig;
